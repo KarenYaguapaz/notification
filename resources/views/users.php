e91498994bdce70a2cfd920f3266ba71ffbfcc39
KarenYaguapaz <32964453+KarenYaguapaz@users.noreply.github.com>
2018-01-24 21:16:28 -0500

    Aprendizaje
    Vistas

---
+++ resources/views/users.php
@@ -0,0 +1,13 @@
+<!doctype html>
+ <html lang="en">
+ <head>
+ 	<meta charset="UTF-8">
+ 	<meta name:"viewport"
+ 	content="witdth=divice-width, users-scalable=no, initial-scale=1.0, maximun-scales=1.0, minimun-scales=1.0">
+ 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+ 	<title>Listado de usuarios CLADIAN</title>
+</head>
+<body>
+	<h1> Usuarios</h1>
+</body>
+</html>
