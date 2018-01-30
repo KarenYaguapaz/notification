963d85e068ae0b1eb3bfdb74126f10751b1b1fee
KarenYaguapaz <32964453+KarenYaguapaz@users.noreply.github.com>
2018-01-30 17:50:11 -0500

    Vistas
    Creacion de vistas

--- app/PasswordReset.php
+++ app/PasswordReset.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class PasswordReset extends Model
 {
-    protected $table= 'users';
+    protected $table = 'PasswordReset';
     protected $primaryKey= 'id';
     protected $timestamps= false;
 }
