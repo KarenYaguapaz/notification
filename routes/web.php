24f01d4389efcab5f9b68bb50287d1e8b3b73a47
KarenYaguapaz <32964453+KarenYaguapaz@users.noreply.github.com>
2018-01-31 21:14:57 -0500

    Ruta
    Ruta usuarios

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,6 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
+Route::get('/')
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::view('users', 'Usuarios')
