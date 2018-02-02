0b0833e53b416a4fafd5ea558836226c33d01eb5
KarenYaguapaz <32964453+KarenYaguapaz@users.noreply.github.com>
2018-02-01 19:13:03 -0500

    Controllers
    creacion de controladores:  notificatio y users, en el archivo de HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,4 +25,12 @@ class HomeController extends Controller
     {
         return view('home');
     }
+    public function notification()
+    {
+      return view('notifications');
+    }
+    public function user()
+    {
+      return view('users');
+    }
 }
