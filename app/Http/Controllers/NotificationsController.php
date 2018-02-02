d658c78e67aa32ded9b0d72f74d0bef82ef1f783
KarenYaguapaz <32964453+KarenYaguapaz@users.noreply.github.com>
2018-02-01 19:16:14 -0500

    Controller
    Está comentado porque no se conecta con la ruta

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -1,10 +1,31 @@
 <?php
 
-namespace App\Http\Controllers;
+/*namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
 class NotificationsController extends Controller
 {
-    //
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    //public function __construct()
+    //{
+      //  $this->middleware('auth');
+    //}
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    //public function index()
+    //{
+      //  return view('home');
+    //}
+    //public function notification(){
+      //return view('notifications');
+    //}
 }
