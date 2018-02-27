ee9b51909c87d61e2178f4b2023cc90856f7a2f3
KarenYaguapaz <32964453+KarenYaguapaz@users.noreply.github.com>
2018-02-26 21:29:49 -0500

    NotificationsControler
    Se puso el controlador

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -1,31 +1,17 @@
 <?php
 
-/*namespace App\Http\Controllers;
+namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
 class NotificationsController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    //public function __construct()
-    //{
-      //  $this->middleware('auth');
-    //}
+   public function __construct()
+    {
+        $this->middleware('auth');
+    }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    //public function index()
-    //{
-      //  return view('home');
-    //}
-    //public function notification(){
-      //return view('notifications');
-    //}
+    public function index(){
+        return view ('notifications');
+    }
 }
