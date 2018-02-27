bba1208930d95ef0d5d1ba434905d942733b1cb6
KarenYaguapaz <32964453+KarenYaguapaz@users.noreply.github.com>
2018-02-26 21:31:48 -0500

    user(modelo)
    Se aumento una relacion con notificaciones

--- app/User.php
+++ app/User.php
@@ -26,4 +26,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function Notifications(){
+        return $this->hasMany('App\Notification');
+    }
 }
