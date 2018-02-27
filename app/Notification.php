160669f73435b510fb1ce1fcf7071b11f366731a
KarenYaguapaz <32964453+KarenYaguapaz@users.noreply.github.com>
2018-02-26 21:31:08 -0500

    Notification(Modelo)
    creo modelo de notificacion

--- app/Notification.php
+++ app/Notification.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Notification extends Model
 {
-    //
+	//A que tabla hace referencia este modelo
+    protected $table ='notification';
+
+    //Los campos que son asignables masivamente
+    protected $fillable = [
+        'id', 'title', 'description',
+    ];
+
+    public function user(){
+    	return $this->belogsTo('App\User');
+    }
 }
