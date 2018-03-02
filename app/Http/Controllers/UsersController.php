91020e576a31dbafd9fb254dd6bd2951956e802e
KarenYaguapaz <32964453+KarenYaguapaz@users.noreply.github.com>
2018-03-01 22:21:44 -0500

    Controlador users
    index seeder

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,12 +10,15 @@ class UsersController extends Controller
     {
         $this->middleware('auth');
     }
-    public function index(){
-    	return view('users');
+    //public function index(){
+    //	return view('users');
     }
     //public function show($users){
     	//$usr = Users::find ($users); 
         //dd($usr->name);
         //resturn view('users', compact('usr'));
-    }
-    
+    //}
+    public function index() {
+       return view('users.index', ['user' => User::get()]);
+   }
+}
