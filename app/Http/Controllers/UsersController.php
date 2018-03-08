495223cad4be23c5193ebc7a9ebe7401de8a7e35
KarenYaguapaz <32964453+KarenYaguapaz@users.noreply.github.com>
2018-03-07 22:36:14 -0500

    User Controller
    se volvio solo a index

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,15 +10,8 @@ class UsersController extends Controller
     {
         $this->middleware('auth');
     }
-    //public function index(){
-    //	return view('users');
+    public function index(){
+    	return view('users');
     }
-    //public function show($users){
-    	//$usr = Users::find ($users); 
-        //dd($usr->name);
-        //resturn view('users', compact('usr'));
-    //}
-    public function index() {
-       return view('users.index', ['user' => User::get()]);
-   }
+   
 }
