54c8bdd654ec6a3bc45f9797bd988610d79b3578
KarenYaguapaz <32964453+KarenYaguapaz@users.noreply.github.com>
2018-03-11 18:40:12 -0500

    page-seeder

---
+++ database/seeds/PagesTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+
+class PagesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+        foreach (range(1, 10) as $index) {
+            DB::table('pages')->insert([
+                'title' => $faker->sentence,
+                'description' => $faker->paragraph,
+                'image' => $faker->image('images',400,300),
+            ]);
+        }
+    }
+}
